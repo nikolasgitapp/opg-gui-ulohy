1155b78237e8376705f78dd82485e9a6f03dea68
LiptakN <liptaknikolas10@gmail.com>
2026-02-16 11:33:32 +0100

    uloha 1 dokoncena

--- src/sk/spse/uloha1/declarative/Controller.java
+++ src/sk/spse/uloha1/declarative/Controller.java
@@ -2,6 +2,7 @@ package sk.spse.uloha1.declarative;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 /**
  * Controller pre FXML súbor – obsahuje logiku aplikácie
@@ -12,10 +13,34 @@ public class Controller {
 
     @FXML
     private TextField counterField;
+    @FXML
+    private TextField cfield;
+    @FXML
+    private TextField ffield;
 
     @FXML
     private void incrementCounter() {
         counter++;
         counterField.setText(String.valueOf(counter));
+    }
+    @FXML
+    private void convertc() {
+        try{
+        double c = Double.parseDouble(cfield.getText());
+        double f = (1.8 * c) + 32;
+        ffield.setText(Double.toString(f)); }
+        catch (NumberFormatException e) {
+
+        }
+    }
+    @FXML
+    public void convertt() {
+        try {
+        double f = Double.parseDouble(ffield.getText());
+        double c = (f - 32) / 1.8;
+        cfield.setText(Double.toString(c)); }
+        catch (NumberFormatException e) {
+
+        }
     }
 }
--- src/sk/spse/uloha1/procedural/Application.java
+++ src/sk/spse/uloha1/procedural/Application.java
@@ -1,12 +1,17 @@
 package sk.spse.uloha1.procedural;
 
+import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.awt.event.KeyEvent;
 
 ///
 /// Trieda pre procedurálne vytvorené GUI
@@ -15,29 +20,58 @@ import javafx.stage.Stage;
 ///
 
 public class Application extends javafx.application.Application {
-
-    private int counter = 0;
-
+    private TextField cfield;
+    private TextField ffield;
     @Override
-    public void start(Stage stage) {
-        Label label = new Label("Počítadlo:");
+    public void start(Stage primaryStage) {
 
-        TextField textField = new TextField("0");
+        Label lblC = new Label("Stupne Celsia:");
+        cfield = new TextField("0");
+        Label lblCUnit = new Label("°C:");
 
-        Button button = new Button("Plus 1");
+        HBox rowC = new HBox( 5, lblC, cfield, lblCUnit);
+        rowC.setAlignment(Pos.CENTER_RIGHT);
+        rowC.setPadding(new Insets(10));
 
-        button.setOnAction(e -> {
-            counter++;
-            textField.setText(String.valueOf(counter));
-        });
+        Label lblF = new Label("Stupne Farenheita:");
+        ffield = new TextField("0");
+        Label lblFUnit = new Label("°F:");
 
-        HBox root = new HBox(20, label, textField, button);
-        root.setPadding(new Insets(10));
+        HBox rowF = new HBox( 5, lblF, ffield, lblFUnit);
+        rowF.setAlignment(Pos.CENTER_RIGHT);
+        rowF.setPadding(new Insets(10));
 
-        Scene scene = new Scene(root);
+        //Hlavný kontajner
+        VBox root = new VBox( 0, rowC, rowF);
 
-        stage.setTitle("Procedural Application 1");
-        stage.setScene(scene);
-        stage.show();
+        Scene scene = new Scene(root, 420, 120);
+
+        cfield.setOnKeyTyped(  e -> convertc());
+        ffield.setOnKeyTyped(  e -> convertt());
+
+        primaryStage.setTitle("Procedural Application 1");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @FXML
+    private void convertc() {
+        try{
+            double c = Double.parseDouble(cfield.getText());
+            double f = (1.8 * c) + 32;
+            ffield.setText(Double.toString(f)); }
+        catch (NumberFormatException e) {
+
+        }
+    }
+    @FXML
+    public void convertt() {
+        try {
+            double f = Double.parseDouble(ffield.getText());
+            double c = (f - 32) / 1.8;
+            cfield.setText(Double.toString(c)); }
+        catch (NumberFormatException e) {
+
+        }
     }
 }
